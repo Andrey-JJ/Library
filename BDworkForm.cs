e3880b9cd6785fb3d5453e6b760581317d184e36
Андрей Орехов <andrey.0201@hotmail.com>
2022-11-15 11:32:26 +0300

    Добавление вывода конкретных значений

--- BDworkForm.cs
+++ BDworkForm.cs
@@ -51,28 +51,29 @@ namespace Library
             switch (s)
             {
                 case "Экземпляр книги":
-                    temp = "copy_of_book";
+                    temp = "select book_name, book_sub from book, book_card where book_card_id = bcard_id order by book_id";
                     break;
                 case "Каталожная карточка книги":
-                    temp = "catalog_card_of_book";
+                    temp = "select book_name, book_author, book_edit, book_vol, dep_name from book_card, department where book_dep_id = dep_id order by bcard_id";
                     break;
                 case "Выдача":
-                    temp = "book_issue";
+                    temp = "select * from book_issue";
                     break;
                 case "Отдел":
-                    temp = "department";
+                    temp = "select dep_id, dep_name from department order by dep_id";
                     break;
                 case "Абонент":
-                    temp = "subscriber";
+                    temp = "select sub_lastname, sub_name, sub_midname from subscriber order by sub_id";
                     break;
                 case "Библиотекарь":
-                    temp = "librarian";
+                    temp = "select lib_lastname, lib_name, lib_midname from librarian order by lib_id";
                     break;
             }
-            string table = $"select * from {temp}";
+            //string table = $"select * from {temp}";
+            //string temptable = temp;
             try
             {
-                NpgsqlCommand command = new NpgsqlCommand(table, conn);
+                NpgsqlCommand command = new NpgsqlCommand(temp, conn);
                 NpgsqlDataReader reader = command.ExecuteReader();
                 DataTable dt = new DataTable();
                 dt.Load(reader);
